560332b7f6dfbeca6c54ebe712230d4e64e178f1
Gabriele <g.nocchi@hotmail.com>
2023-03-01 12:52:07 -0700

    Add files via upload

--- jim.R
+++ jim.R
@@ -10,7 +10,10 @@ df_reshaped <- dcast(df, pop ~ chr + bp, value.var = "alt_freq")
 # Rename the first column to "ID"
 colnames(df_reshaped)[1] <- "ID"
 
-df_reshaped <- df_reshaped[ , colSums(is.na(df_reshaped))==0]
+# select columns(SNP) with max 10% NA
+df_reshaped <- df_reshaped[, colMeans(is.na(df_reshaped)) <= 0.1]
+
+
 
 # Get the column names of the data frame (excluding the first column)
 cols <- colnames(df_reshaped)[-1]
@@ -21,4 +24,17 @@ sampled_cols <- sample(cols, 10000, replace = FALSE)
 # Subset the data frame with the sampled columns
 sampled_df <- df_reshaped[, c("ID", sampled_cols)]
 
-write.table(sampled_df, file ="snp.forR", row.names = F, quote =F, sep ="\t")
+
+
+
+# Impute the NA and give them the Median freq
+library(zoo)
+library(dplyr)
+new <- sampled_df %>% select(-1) %>% na.aggregate(FUN = median, na.rm = FALSE)
+ID <- sampled_df[,1]
+new <- data.frame(ID, new, stringsAsFactors = FALSE)
+
+
+
+write.table(new, file ="snp.forR", row.names = F, quote = F, sep ="\t")
+
--- r.sh
+++ r.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=0-6:00
+#SBATCH --time=0-12:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=64G
+#SBATCH --mem-per-cpu=128G
 #SBATCH --account=def-yeaman
 
 module load r/4.2.1
